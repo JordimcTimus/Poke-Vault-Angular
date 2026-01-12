897591c664b7679149ec50120f169ad19b670d30
JordimcTimus <jordi.munozcursa@institutvidreres.cat>
2026-01-12 10:59:28 +0100

    Copia un poco "funcional"2

---
+++ src/app/capcelera/capcelera.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capcelera',
+  imports: [],
+  templateUrl: './capcelera.html',
+  styleUrl: './capcelera.css',
+})
+export class Capcelera {
+
+}
---
+++ src/app/terminos-de-uso/terminos-de-uso.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terminos-de-uso',
+  imports: [],
+  templateUrl: './terminos-de-uso.html',
+  styleUrl: './terminos-de-uso.css',
+})
+export class TerminosDeUso {
+
+}
