0b8a85c522c8f9f677e703c1869edfab7ec8775a
JordimcTimus <jordi.munozcursa@institutvidreres.cat>
2026-01-27 13:15:05 +0100

    he echo lo que he podido

--- src/app/cateleg-cartes/cateleg-cartes.ts
+++ src/app/cateleg-cartes/cateleg-cartes.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {NgOptimizedImage} from '@angular/common';
 import {RouterLink} from '@angular/router';
 import {Capcelera} from '../capcelera/capcelera';
+import {Productes} from '../services/productes';
 
 @Component({
   selector: 'app-cateleg-cartes',
