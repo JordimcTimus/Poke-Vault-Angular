8600460b1b2761b9989d35de5ad0347c3cedc17c
Kevin T <kevin.timus@institutvidreres.cat>
2026-05-11 11:43:11 +0200

    A5

--- src/Backend/models/init-models.js
+++ src/Backend/models/init-models.js
@@ -2,21 +2,20 @@ var DataTypes = require("sequelize").DataTypes;
 var _comandes       = require("./comandes");
 var _linies_comanda = require("./linies_comanda");
 var _producte       = require("./producte");
+var _carrito        = require("./carrito");
 
 function initModels(sequelize) {
   var comandes       = _comandes(sequelize, DataTypes);
   var linies_comanda = _linies_comanda(sequelize, DataTypes);
   var producte       = _producte(sequelize, DataTypes);
+  var carrito        = _carrito(sequelize, DataTypes);
 
-  // Una comanda tiene muchas linies, una linia pertenece a una comanda
-  linies_comanda.belongsTo(comandes, { as: "comanda",  foreignKey: "idcomandes" });
-  comandes.hasMany(linies_comanda,   { as: "linies",   foreignKey: "idcomandes" });
+  linies_comanda.belongsTo(comandes,  { as: "comanda",  foreignKey: "idcomandes" });
+  comandes.hasMany(linies_comanda,    { as: "linies",   foreignKey: "idcomandes" });
+  linies_comanda.belongsTo(producte,  { as: "producte", foreignKey: "idproducte" });
+  producte.hasMany(linies_comanda,    { as: "linies",   foreignKey: "idproducte" });
 
-  // Un producte tiene muchas linies, una linia pertenece a un producte
-  linies_comanda.belongsTo(producte, { as: "producte", foreignKey: "idproducte" });
-  producte.hasMany(linies_comanda,   { as: "linies",   foreignKey: "idproducte" });
-
-  return { comandes, linies_comanda, producte };
+  return { comandes, linies_comanda, producte, carrito };
 }
 
 module.exports = initModels;
